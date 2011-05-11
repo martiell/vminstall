09c66b7e6a35e11bbdbde15ae38089b64082f87b
Martin Ellis <martin@contemplateltd.com>
2011-05-11 17:08:12 +0100

    Check whether vboxmanage binary is VBoxManage (OS X) or vboxmanage (Linux)

--- vm/vm.sh
+++ vm/vm.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -e
 #wget -N http://ftp.uk.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/netboot/mini.iso
 #wget -N http://ftp.uk.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/netboot/netboot.tar.gz
-VBoxManage createvm --name martin --ostype "Debian_64" --register
-VBoxManage storagectl martin --name "SATA Controller" --add sata
-VBoxManage createhd --filename martin-sda --size 500
-VBoxManage storageattach martin --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium martin-sda.vdi
-VBoxManage storageattach martin --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium cd.iso
-VBoxManage modifyvm martin --keyboard usb
+
+config() {
+  for d in vboxmanage VBoxManage ; do
+    which $d > /dev/null && VBOXMANAGE=$d && break
+  done
+}
+
+config
+$VBOXMANAGE createvm --name martin --ostype "Debian_64" --register
+$VBOXMANAGE storagectl martin --name "SATA Controller" --add sata
+$VBOXMANAGE createhd --filename martin-sda --size 500
+$VBOXMANAGE storageattach martin --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium martin-sda.vdi
+$VBOXMANAGE storageattach martin --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium cd.iso
+$VBOXMANAGE modifyvm martin --keyboard usb
 #VBoxManage unregistervm --delete martin
 #VBoxManage startvm martin --type headless
